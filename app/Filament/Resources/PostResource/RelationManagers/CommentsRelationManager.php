feb2e4518dcc3a4f0ec180c9633c788d834ac516
Rami Abo Ajeeb <rami@zeour.co.uk>
2023-11-23 14:48:08 +0300

    comment & popular posts

---
+++ app/Filament/Resources/PostResource/RelationManagers/CommentsRelationManager.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Filament\Resources\PostResource\RelationManagers;
+
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Filament\Forms\Components\Select;
+use Filament\Tables\Columns\TextColumn;
+
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class CommentsRelationManager extends RelationManager
+{
+    protected static string $relationship = 'comments';
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('comment')
+                    ->required()
+                    ->maxLength(255),
+                Select::make('user_id')
+                    ->relationship('user','name')
+                    ->searchable()
+                    ->preload()
+                    ->required(),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('comment')
+            ->columns([
+                Tables\Columns\TextColumn::make('comment'),
+                TextColumn::make('user.name')->sortable()->searchable(),
+
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                Tables\Actions\CreateAction::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+
+            ])
+            ->emptyStateActions([
+                Tables\Actions\CreateAction::make(),
+            ]);
+    }
+}
